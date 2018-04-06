2c8e3187e84ad695d9a215cf514e6a933ed95f20
colining <18810782954@163.com>
2018-04-06 21:48:18 +0800

    complete InverseReduce and MyMap

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -3,7 +3,9 @@ package com.thoughtworks.collection;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MyMap {
 
@@ -17,22 +19,38 @@ public class MyMap {
     }
 
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+        return array.stream().map(integer -> integer * 3).collect(Collectors.toList());
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+        return array.stream().map(integer -> String.valueOf((char) (integer + 96))).collect(Collectors.toList());
     }
 
     public List<String> mapLetters() {
-        throw new NotImplementedException();
+        return array.stream().map(integer -> {
+            StringBuilder stringBuilder = new StringBuilder();
+            while (integer > 26) {
+                int i = integer/26;
+                integer%=26;
+                if (integer == 0) {
+                    return stringBuilder+String.valueOf((char) (i-1 + 96))+"z";
+                }
+                stringBuilder.append(String.valueOf((char) (i + 96)));
+            }
+            return stringBuilder+String.valueOf((char) (integer + 96));
+        }).collect(Collectors.toList());
     }
 
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+        return array.stream().sorted(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2-o1;
+            }
+        }).collect(Collectors.toList());
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+        return array.stream().sorted().collect(Collectors.toList());
     }
 }
