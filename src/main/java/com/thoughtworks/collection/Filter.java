0d09ab0467b4b766d9f55a2e5201e83ac766e79e
colining <18810782954@163.com>
2018-04-06 20:39:39 +0800

    complete Filter and Flaten

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -1,9 +1,12 @@
 package com.thoughtworks.collection;
 
+import com.sun.org.apache.regexp.internal.RE;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Filter {
 
@@ -14,18 +17,32 @@ public class Filter {
     }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+        List<Integer> integers;
+        integers = array.stream().filter(integer -> integer % 2 == 0).collect(Collectors.toList());
+        return integers;
     }
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
+        return array.stream().filter(integer -> integer % 3 == 0).collect(Collectors.toList());
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
+        return firstList.stream().filter(integer -> {
+            for (Integer integer1 : secondList) {
+                if (integer == integer1)
+                    return true;
+            }
+            return false;
+        }).collect(Collectors.toList());
     }
 
     public List<Integer> getDifferentElements() {
-        throw new NotImplementedException();
+        ArrayList<Integer> integers = new ArrayList<>();
+        for (int i = 0; i < array.size(); i++) {
+            if (i == array.size() - 1 || array.get(i) != array.get(i + 1)) {
+                integers.add(array.get(i));
+            }
+        }
+        return integers;
     }
 }
--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -1,21 +1,39 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class Flaten {
 
     Integer[][] array;
+
     public Flaten(Integer[][] array) {
         this.array = array;
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> integers = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            for (int i1 = 0; i1 < array[i].length; i1++) {
+                integers.add(array[i][i1]);
+            }
+        }
+        return integers;
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> integers = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            for (int i1 = 0; i1 < array[i].length; i1++) {
+                if (!integers.contains(array[i][i1]))
+                    integers.add(array[i][i1]);
+            }
+        }
+        return integers;
+    }
+
+    public static void main(String[] args) {
+        int[][] test = new int[][]{{1}, {2}, {3}, {4, 5}};
+        System.out.println(test[3][1]);
     }
 }
